7c6f1a0b20a7422b9b8704955316822c7ac5b86b
Bismark Aidoo <mc07.aidoo@gmail.com>
2023-10-29 08:12:11 +0000

    The challenge

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -35,5 +35,9 @@ if __name__ == '__main__':
         print("Example: ./0-fizzbuzz.py 89")
         sys.exit(1)
 
-    number = int(sys.argv[1])
-    fizzbuzz(number)
+    try:
+        number = int(sys.argv[1])
+        fizzbuzz(number)
+    except ValueError:
+        print("Invalid input. Please provide a valid number.")
+        sys.exit(1)
